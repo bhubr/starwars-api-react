2ab9061f3fee8c29f6d27bf75aedee62d22f82bb
Benoît Hubert <benoithubert@gmail.com>
2019-10-14 13:51:39 +0200

    Updates

---
+++ precache-manifest.4fdc5a98f2427d681a93728a0f832fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d405b950a1a9986efce1062e8c87267b",
+    "url": "/starwars-api-react/index.html"
+  },
+  {
+    "revision": "a7b5dec0efaf3186e6d9",
+    "url": "/starwars-api-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9275499ec944c1274cf7",
+    "url": "/starwars-api-react/static/js/2.fa503eeb.chunk.js"
+  },
+  {
+    "revision": "a7b5dec0efaf3186e6d9",
+    "url": "/starwars-api-react/static/js/main.f28bbb3a.chunk.js"
+  },
+  {
+    "revision": "073bb2ee4c1b537039ba",
+    "url": "/starwars-api-react/static/js/runtime-main.d0e88564.js"
+  }
+]);
